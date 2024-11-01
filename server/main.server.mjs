0180daea6fef4323debdfce8b5c5dba7f57a9347
Juan Sebastian Sanin <juans.saninv@uniquindio.edu.co>
2024-11-01 16:20:02 -0500

    Auto-generated commit

---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2HSAO7KS.mjs";import"./chunk-DBEEF2UL.mjs";import"./chunk-NRK35GIP.mjs";import"./chunk-J6MPR3F6.mjs";import"./chunk-MFFGDCWH.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
