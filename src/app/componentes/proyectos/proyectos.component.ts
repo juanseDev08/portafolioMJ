b840d06007ea969cd2548d466effb803ce1031a7
Juan Sebastian Sanin <juans.saninv@uniquindio.edu.co>
2024-11-01 16:08:25 -0500

    Complemento y correcta distribución de los proyectos

--- src/app/componentes/proyectos/proyectos.component.ts
+++ src/app/componentes/proyectos/proyectos.component.ts
@@ -8,18 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class ProyectosComponent implements OnInit{
   proyectos = [
     {
-      nombre: 'Vitale consciente',
-      descripcion: 'Página en construcción para la empresa Vitale consciente',	
-      imagen: 'assets/images/vitale.png',
-      link: 'En construcción'
+      nombre: 'Gestión de proyectos de extensión',
+      descripcion: 'Proyecto interno de la universidad del quidío(no es permitido compartir el código, ni más información relevante)',
+      imagen: 'assets/images/GPE.png',
+      link: 'no disponible',
+      disable: true,
     },
     {
-      nombre: 'Gestión de proyectos de extensión',
-      descripcion: 'Proyecto interno de la universidad del quidío(no es permitido compartir el código) ni más información relevante',
-      imagen: 'assets/images/GPE.png',
-      link: 'no disponible'
+      nombre: 'Portafolio en Angular 17',
+      descripcion: 'Este portafolio es creado directamente por mi, con el fin de mostrar mis proyectos y habilidades,no fue creado por medio de guías, fue creado desde 0',
+      imagen: 'assets/images/portafolioAngular.png',
+      link: 'https://github.com/juanseDev08/portafolioMJ',
     },
-    // Agrega más proyectos según sea necesario
+    {
+      nombre: 'Portafolio en Astro',
+      descripcion: 'Este es un portafolio creado por medio de guías de Astro, con el fin de mostrar mis proyectos y habilidades(no fue creado propiamente por mi, fue más basado a un código guía)',	
+      imagen: 'assets/images/portafolioAstro.png',
+      link: 'https://github.com/juanseDev08/portafolio.dev?tab=readme-ov-file',
+      disable: false,
+    },
+    {
+      nombre: 'Vitale consciente',
+      descripcion: 'Página en construcción para la empresa Vitale consciente (no hay github disponible)',	
+      imagen: 'assets/images/vitale.png',
+      link: 'En construcción',
+      disable: true,
+    },
   ];
 
   constructor() { }
